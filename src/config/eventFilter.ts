0980f06d29773698fbe7f14b8216e6558760aa7a
Allen Zheng <zhenga8533@gmail.com>
2025-11-11 10:09:36 -0500

    Standardize quotes and update event categories
    Replaced double quotes with single quotes throughout the file for consistency. Expanded the 'Major Events' category group with additional event types such as 'City Safari', 'GO Fest', 'GO Tour', 'Max Battles', and 'Research Day'.

--- src/config/eventFilter.ts
+++ src/config/eventFilter.ts
@@ -1,5 +1,5 @@
 export const initialFilters = {
-  searchTerm: "",
+  searchTerm: '',
   selectedCategories: [] as string[],
   startDate: null as Date | null,
   endDate: null as Date | null,
@@ -10,24 +10,36 @@ export const initialFilters = {
 };
 
 export const marks = [
-  { value: 0, label: "12 AM" },
-  { value: 6, label: "6 AM" },
-  { value: 12, label: "12 PM" },
-  { value: 18, label: "6 PM" },
-  { value: 24, label: "12 AM" },
+  { value: 0, label: '12 AM' },
+  { value: 6, label: '6 AM' },
+  { value: 12, label: '12 PM' },
+  { value: 18, label: '6 PM' },
+  { value: 24, label: '12 AM' },
 ];
 
 export const dayOptions = [
-  { value: 0, label: "Sunday" },
-  { value: 1, label: "Monday" },
-  { value: 2, label: "Tuesday" },
-  { value: 3, label: "Wednesday" },
-  { value: 4, label: "Thursday" },
-  { value: 5, label: "Friday" },
-  { value: 6, label: "Saturday" },
+  { value: 0, label: 'Sunday' },
+  { value: 1, label: 'Monday' },
+  { value: 2, label: 'Tuesday' },
+  { value: 3, label: 'Wednesday' },
+  { value: 4, label: 'Thursday' },
+  { value: 5, label: 'Friday' },
+  { value: 6, label: 'Saturday' },
 ];
 
 export const categoryGroups = {
-  "Major Events": ["Community Day", "Pokémon GO Fest", "Pokémon GO Tour", "Raid Day", "Raid Weekend", "Wild Area"],
-  "Weekly Events": ["Raid Hour", "Pokémon Spotlight Hour", "Max Mondays"],
+  'Major Events': [
+    'City Safari',
+    'Community Day',
+    'GO Fest',
+    'GO Tour',
+    'Max Battles',
+    'Pokémon GO Fest',
+    'Pokémon GO Tour',
+    'Raid Day',
+    'Raid Weekend',
+    'Research Day',
+    'Wild Area',
+  ],
+  'Weekly Events': ['Raid Hour', 'Pokémon Spotlight Hour', 'Max Mondays'],
 };
